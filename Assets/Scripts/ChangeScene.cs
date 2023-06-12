80de887c1c4258435750272a1a09c6ee6b717bf4
nicorm <nicorm2000@gmail.com>
2023-06-12 00:19:37 -0300

    Created scene transitions, re organised the animation folder and created new animations for the transitions

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,18 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
     [SerializeField] int sceneToChangeTo;
+    [SerializeField] float waitTimeBetweenScenes = 1.5f;
+    [SerializeField] Animator transitionAnim;
     [SerializeField] LayerMask playerLayer;
 
     private void OnTriggerEnter(Collider other)
     {
         if (((1 << other.gameObject.layer) & playerLayer) != 0)
         {
-            SceneManager.LoadScene(sceneToChangeTo);
+            StartCoroutine(LoadScene());
         }
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("End");
+
+        yield return new WaitForSeconds(waitTimeBetweenScenes);
+
+        SceneManager.LoadScene(sceneToChangeTo);
     }
 }
